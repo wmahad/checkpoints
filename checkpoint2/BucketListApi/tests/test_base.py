dc07e93ce8da20520701bfd0eb4815cd6361ccc4
mahad <engmk123@gmail.com>
2015-12-14 10:32:27 +0300

    documented all tests

--- checkpoint2/BucketListApi/tests/test_base.py
+++ checkpoint2/BucketListApi/tests/test_base.py
@@ -8,18 +8,22 @@ class BaseTestCase(TestCase):
     """A base test case for flask-tracking."""
 
     def create_app(self):
+        """method for setting up config variables for the app"""
         app.config.from_object('config.TestingConfig')
         return app
 
     def setUp(self):
+        """setUp method"""
         self.app = app.test_client()
         db.create_all()
 
     def tearDown(self):
+        """method for clearing all settings"""
         db.session.remove()
         db.drop_all()
 
     def request(self, method, url, auth=None, **kwargs):
+        """method to use to represent Authorization means for the tests"""
         headers = kwargs.get('headers', {})
         if auth:
             headers['Authorization'] = 'Basic ' + \
